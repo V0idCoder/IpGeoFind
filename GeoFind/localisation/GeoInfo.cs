22e74cd59dae6f0914505f91895f15f615591bda
V0idCoder <114491282+V0idCoder@users.noreply.github.com>
2023-01-26 14:37:49 +0100

    First Upload

---
+++ GeoFind/localisation/GeoInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace localisation
+{
+    class GeoInfo
+    {
+        public string Query { get; set; }
+        public string Status { get; set; }
+        public string Country { get; set; }
+        public string ZIP { get; set; }
+        public String City { get; set; }
+
+        [JsonPropertyName("as")]    // nécessite System.Text.Json.Serialization
+        public string AutonomousSystem { get; set; }
+
+
+    }
+}
---
+++ GeoFind/localisation/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using localisation;
+using System.Net.Http;
+using System.Net.Http.Json;
+
+while (true) {
+    HttpClient httpClient = new();
+    Console.Write("Enter an IP address: ");
+    var ip = Console.ReadLine();
+    if (String.IsNullOrWhiteSpace(ip))
+    {
+        break;
+    }
+var uri = $"http://ip-api.com/json/{ip}";
+var info = httpClient.GetFromJsonAsync<GeoInfo>(uri).Result;
+    Console.WriteLine($" Status: {info.Status}\r\n Country: {info.Country}\r\n City: {info.City}\r\n ZIP: {info.ZIP}\r\n Query: {info.Query} \r\n");
+}    
